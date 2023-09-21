d4962f8db2bc4f9b52e0a75be185bb4698119342
alexxk2 <alexx.koda@gmail.com>
2023-09-21 16:51:39 +0300

    Initial commit. Dependencies added. Single activity pattern added.

---
+++ app/src/main/java/com/example/shoppinglist/presentation/details/DetailsFragment.kt
@@ -0,0 +1,45 @@
+package com.example.shoppinglist.presentation.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.FragmentDetailsBinding
+import com.example.shoppinglist.databinding.FragmentListsBinding
+
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentDetailsBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/presentation/lists/ListsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.shoppinglist.presentation.lists
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.FragmentListsBinding
+
+
+class ListsFragment : Fragment() {
+    private var _binding: FragmentListsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListsBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
